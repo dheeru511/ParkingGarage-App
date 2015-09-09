5710ed2e978c8c8fd4dae4d01c4575f908625b71
Dheeraj <dheerajsingh@dheerajs-macbook-air.local>
2015-09-09 09:27:31 -0500

    Initial Commit

---
+++ ParkingApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ParkingApp
+//
+//  Created by Dheeraj Singh on 9/9/15.
+//  Copyright © 2015 Dheeraj Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ParkingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParkingApp
+//
+//  Created by Dheeraj Singh on 9/9/15.
+//  Copyright © 2015 Dheeraj Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
